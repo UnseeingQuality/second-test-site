79911d2ebc05daf296181e7a96b8eb66285f45ef
UnseeingQuality <balakinaa2016@mail.ru>
2023-05-14 17:28:50 +0500

    message

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { header-title } from './Styles/Header.css';
+
+function Header() {
+  return (
+    <h1 className="header-title">Press Start 2P Header</h1>
+  );
+}
+
+export default Header;
